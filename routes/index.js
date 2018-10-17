c1f47dbaf029e183cac44f4b880286f21f40a6a7
Nidhi Pai <nidhi.pai@gmail.com>
2018-10-17 12:45:27 -0400

    header pt 1

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,41 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Accuracy' });
 });
 
+router.get('/company', function(req, res, next) {
+  res.render('company', {title: 'Company | Accuracy'});
+})
+
+router.get('/team', function(req, res, next) {
+  res.render('team', {title: 'Team | Accuracy'});
+})
+
+router.get('/testimonials', function(req, res, next) {
+  res.render('testimonials', {title: 'Testimonials | Accuracy'});
+})
+
+router.get('/calendar', function(req, res, next) {
+  res.render('calendar', {title: 'Calendar | Accuracy'});
+})
+
+router.get('/sp', function(req, res, next) {
+  res.render('sp', {title: 'Services and Pricing | Accuracy'});
+})
+
+router.get('/terms', function(req, res, next) {
+  res.render('terms', {title: 'Terms of Service | Accuracy'});
+})
+
+router.get('/contactus', function(req, res, next) {
+  res.render('contactus', {title: 'Contact Us | Accuracy'});
+})
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq', {title: 'FAQ | Accuracy'});
+})
+
+router.get('/sitemap', function(req, res, next) {
+  res.render('sitemap', {title: 'Sitemap | Accuracy'});
+})
+
+
 module.exports = router;
